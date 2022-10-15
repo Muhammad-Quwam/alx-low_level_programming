ba881eb9fa8f0990d4c349f91fadb2dbf47a37ee
Muhammad-Quwam <quwamhassan9@gmail.com>
2022-10-15 04:58:05 -0700

    Task one adjustment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,12 @@
 /**
  * main - print if the number is negative, positive or 0
  * Return: Always 0
- */
+*/
 int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
